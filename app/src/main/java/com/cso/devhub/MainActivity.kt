ba5795f533075a18ea4be6c364156b9b7566214a
Cainã Oliveira <csoliveira01@gmail.com>
2024-04-30 19:38:03 -0400

    init project

---
+++ app/src/main/java/com/cso/devhub/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.cso.devhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.cso.devhub.ui.theme.DevHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DevHubTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomePage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomePage() {
+    Column {
+        Image(painter = painterResource(id = R.drawable.mexican_cat), contentDescription = "")
+        Text(text = "Cainã Oliveira")
+        Text(text = "cainan")
+        Text(text = "Developer")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DevHubTheme {
+        HomePage()
+    }
+}
