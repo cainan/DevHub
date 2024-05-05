b98ad549c9114804ed6ad5e7c6286692239ad97f
Cainã Oliveira <csoliveira01@gmail.com>
2024-05-05 15:46:54 -0400

    Loading image from internet

--- app/src/main/java/com/cso/devhub/MainActivity.kt
+++ app/src/main/java/com/cso/devhub/MainActivity.kt
@@ -3,7 +3,6 @@ package com.cso.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,11 +24,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.cso.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,9 +69,13 @@ fun HomePage() {
                 )
                 .height(boxHeight)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.mexican_cat),
+            AsyncImage(
+                model = ImageRequest.Builder(
+                    LocalContext.current
+                ).data("https://avatars.githubusercontent.com/u/977227?v=4").crossfade(true)
+                    .build(),
                 contentDescription = "",
+                placeholder = painterResource(android.R.drawable.presence_invisible),
                 modifier = Modifier
                     .offset(y = imageHeight / 2)
                     .size(imageHeight)
@@ -95,13 +101,9 @@ fun HomePage() {
                 fontSize = 18.sp,
             )
             Text(
-                text = "Instructor and Software Developer at @",
+                text = "Software Developer at @",
                 modifier = Modifier
-                    .padding(
-                        start = 8.dp,
-                        bottom = 8.dp,
-                        end = 8.dp,
-                    )
+                    .padding(8.dp)
                     .fillMaxWidth(),
                 textAlign = TextAlign.Center,
             )
