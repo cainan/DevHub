0dca170bd198d9164118e4454732302fc2456969
Cainã Oliveira <csoliveira01@gmail.com>
2024-05-05 15:13:59 -0400

    Updating UI

--- app/src/main/java/com/cso/devhub/MainActivity.kt
+++ app/src/main/java/com/cso/devhub/MainActivity.kt
@@ -4,15 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cso.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,10 +52,60 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HomePage() {
     Column {
-        Image(painter = painterResource(id = R.drawable.mexican_cat), contentDescription = "")
-        Text(text = "Cainã Oliveira")
-        Text(text = "cainan")
-        Text(text = "Developer")
+        val boxHeight = remember {
+            150.dp
+        }
+        val imageHeight = remember {
+            boxHeight
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Color.Gray,
+                    shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp)
+                )
+                .height(boxHeight)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.mexican_cat),
+                contentDescription = "",
+                modifier = Modifier
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(CircleShape)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(imageHeight / 2))
+
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Cainã Oliveira",
+                fontSize = 32.sp,
+            )
+            Text(
+                text = "cainan",
+                fontSize = 18.sp,
+            )
+            Text(
+                text = "Instructor and Software Developer at @",
+                modifier = Modifier
+                    .padding(
+                        start = 8.dp,
+                        bottom = 8.dp,
+                        end = 8.dp,
+                    )
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }
 
