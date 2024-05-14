cae830fce29b2bc642b81c155f79116bdaeeba0e
Cainã Oliveira <csoliveira01@gmail.com>
2024-05-13 20:20:15 -0400

    Adding a search box in UI

--- app/src/main/java/com/cso/devhub/MainActivity.kt
+++ app/src/main/java/com/cso/devhub/MainActivity.kt
@@ -3,8 +3,14 @@ package com.cso.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.cso.devhub.ui.screen.ProfileScreen
@@ -16,6 +22,7 @@ class MainActivity : ComponentActivity() {
         private const val TAG = "MainActivity"
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,7 +33,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ProfileScreen(user = "cainan")
+                    Column {
+                        SearchBar(
+                            query = "Search for an user",
+                            onQueryChange = {},
+                            onSearch = {},
+                            active = false,
+                            onActiveChange = {},
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = null
+                                )
+                            }
+                        ) {
+
+                        }
+                        ProfileScreen(user = "cainan")
+                    }
+
                 }
             }
         }
